2872f35d9c3b588fc3dffad084be1e1d461ee3d9
BabyHeyzeus5 <159839380+BabyHeyzeus5@users.noreply.github.com>
2024-10-08 16:22:36 -0500

    Delete public directory

--- public/js/main.js
+++
@@ -1,9 +0,0 @@
-function navFunction() {
-    //Togle navigation menu
-    var x = document.getElementById("navlinks")
-    if (x.style.display === "block") {
-        x.style.display = "none"
-    } else {
-        x.style.display = "block"
-    }
-}
